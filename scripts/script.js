2a5e8370d2aba17e415d9ee2935b4bde90eb45d0
Corey White <white.corey.a@gmail.com>
2016-02-09 18:49:27 -0500

    Starting on controls

--- scripts/script.js
+++ scripts/script.js
@@ -46,17 +46,28 @@ Grid.prototype = {
   }
 };
 
-function HumanController(game) {
+function Controller(game, type) {
   this.game = game;
+  this.type = type;
   this.player = null;
-  this.name = "Human";
 }
-HumanController.prototype = {
-  constructor: HumanController,
+Controller.prototype = {
+  constructor: Controller,
   setPlayer: function(player) {
     this.player = player;
+  },
+  takeTurn: function() {
   }
+};
+
+function HumanController(game) {
+  Controller.call(this, game, "Human");
 }
+HumanController.prototype = Object.create(Controller.prototype);
+HumanController.prototype.constructor = HumanController;
+HumanController.prototype.takeTurn = function() {
+  this.game.display.message(this.player.name + "'s turn!");
+};
 
 function Player(name, marker, controller) {
   this.name = name;
@@ -65,23 +76,41 @@ function Player(name, marker, controller) {
   this.controller.setPlayer(this);
 }
 
-function Game($boardContainer, $playerContainer) {
+function Display($displayContainer) {
+  this.displayContainer = $displayContainer;
+}
+Display.prototype = {
+  constructor: Display,
+  message: function(msg) {
+    this.displayContainer.find("#message").html(msg);
+  }
+};
+
+function Game($boardContainer, $displayContainer) {
   this.board = new Grid(this, 3, $boardContainer);
+  this.display = new Display($displayContainer);
   this.p1 = new Player("Player 1", "X", new HumanController(this));
   this.p2 = new Player("Player 2", "O", new HumanController(this));
   this.curPlayer = this.p1;
 }
-
 Game.prototype = {
   constructor: Game,
   isValidMove: function(cell) {
     return cell.marker === "";
   },
-  move: function(row, col) {
-    var moveCell = this.board.getCell(row, col);
-    if (this.isValidMove(moveCell)) {
-      moveCell.setMarker(this.curPlayer.marker);
-      this.curPlayer = (this.curPlayer === this.p1) ? this.p2 : this.p1;
+  activateCell: function(row, col) {
+    if (this.curPlayer.controller.type === "Human") {
+      this.move(this.board.getCell(row, col));
+    }
+  },
+  togglePlayer() {
+    this.curPlayer = (this.curPlayer === this.p1) ? this.p2 : this.p1;
+    this.curPlayer.controller.takeTurn();
+  },
+  move: function(cell) {
+    if (this.isValidMove(cell)) {
+      cell.setMarker(this.curPlayer.marker);
+      this.togglePlayer();
     }
   }
 };
@@ -91,9 +120,9 @@ Game.prototype = {
 //* Run code when page is ready
 //*********************************************************************
 $(document).ready(function() {
-  var game = new Game($(".board-container"), $(".player-container"));
+  var game = new Game($(".board-container"), $(".display-container"));
 
   $(".board-container").on("click", ".cell", function() {
-    game.move($(this).data("row"), $(this).data("col"));
+    game.activateCell($(this).data("row"), $(this).data("col"));
   })
 });
